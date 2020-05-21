e749d887c3d8e6c9a1a4bed8a090515f2cc8afc7
yingjun.bao <yingjun.bao@fengjr.com>
2020-05-21 19:08:20 +0800

    最长回文子串-dynamic

--- src/main/java/mntm/top/offer/leet/tree/LongestPalindrome.java
+++ src/main/java/mntm/top/offer/leet/tree/LongestPalindrome.java
@@ -1,5 +1,7 @@
 package mntm.top.offer.leet.tree;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @program: perf
  * @description: 最长回文子串
@@ -8,4 +10,43 @@ package mntm.top.offer.leet.tree;
  * @create: 2020-05-21 18:20
  **/
 public class LongestPalindrome {
+
+    public String solution(String s) {
+        if(s.length() < 2){
+            return s;
+        }
+
+        char[] chars = s.toCharArray();
+
+        Boolean[][] dp = new Boolean[s.length()][s.length()];
+
+        for(int i = 0; i < s.length(); i++ ){
+            dp[i][i] = true;
+        }
+
+        int begin = 0;
+        int maxLen = 1;
+
+        for(int j = 1; j < s.length(); j++){
+            for(int i = 0; i < j; i++){
+                if(chars[i] == chars[j]){
+                    if(j - i < 3){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+
+                }else{
+                    dp[i][j] = false;
+                }
+
+                if(dp[i][j] && j - i + 1 > maxLen){
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+
+        return s.substring(begin, begin + maxLen);
+    }
 }
